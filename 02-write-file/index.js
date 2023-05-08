b7c87fef8adcf5c276b452884abf891638c7eba2
Ivan L <visperas1703@gmail.com>
2023-05-08 15:16:48 +0300

    fix: done tasks 02 and 03

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path  = require('path');
+const { stdin, stdout, exit} = require('process');
+const writePath = path.join(__dirname, 'text.txt');
+
+const output = fs.createWriteStream(writePath);
+stdout.write('Введите текст здесь...\n');
+
+function closeText() {
+    output.end(() => {
+        stdout.write('До свидания!');
+        exit();
+    });
+}
+
+stdin.on('data', data => {
+    if (data.toString().trim() === 'exit' ) {
+        closeText();
+    }
+    output.write(data);
+}
+);
+
+
+
+
+
+
---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,30 @@
+
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folderPath, (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  files.forEach((file) => {
+    const filePath = path.join(folderPath, file);
+    fs.stat(filePath, (err, stats) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      if (stats.isFile()) {
+        const fileSizeInBytes = stats.size;
+        const fileSizeInKb = fileSizeInBytes / 1024;
+        const fileExtension = path.extname(filePath).substring(1);
+        const fileName = path.basename(filePath, `.${fileExtension}`);
+        console.log(`${fileName} - ${fileExtension} - ${fileSizeInKb.toFixed(3)}kb`);
+      }
+    });
+  });
+});
